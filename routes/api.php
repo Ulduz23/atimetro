c43d9c7facc262627cebec327097980aecb5df81
Foo Bar <124519337+Ulduz23@users.noreply.github.com>
2023-10-03 11:23:56 +0400

    controller api

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\NewsController;
-use App\Http\Controllers\ContactController;
+use App\Http\Controllers\Api\NewsController;
+use App\Http\Controllers\Api\ContactController;
 
 /*
 |--------------------------------------------------------------------------
